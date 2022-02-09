411b9b70b5b0eec5b92eae0bdcd71b0d2f01bd72
Andreas Börjesson <borjesson.andreas@gmail.com>
2022-02-09 07:26:04 +0100

    Render free lessons on server.

--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -1,31 +1,43 @@
 import PremiumContent from "@components/PremiumContent";
 import Link from "next/link";
 import { useState } from "react";
-import { db } from "@lib/firebase";
-import { query, orderBy, getDocs, collection } from "firebase/firestore";
+import { db, postToJSON } from "@lib/firebase";
+import { query, orderBy, getDocs, collection, where } from "firebase/firestore";
 
-export default function LessonsPage({}) {
+export async function getServerSideProps() {
+  // Load free (non-premium) lessons here
+  const q = query(collection(db, "lessons"), where("premium", "==", false));
+  const lessons = (await getDocs(q)).docs.map(postToJSON);
+
+  return {
+    props: { lessons },
+  };
+}
+
+export default function LessonsPage({ lessons }) {
   return (
     <PremiumContent>
-      <AllLessons />
+      <AllLessons lessons={lessons} />
     </PremiumContent>
   );
 }
 
-function AllLessons() {
-  const [lessons, setLessons] = useState([]);
+function AllLessons({ lessons }) {
+  const [premiumLessons, setPremiumLessons] = useState([]);
   const [fetched, setFetched] = useState(false);
 
-  const getAllLessons = async () => {
-    const snapshot = await getDocs(query(collection(db, "lessons"), orderBy("updatedAt", "desc")));
+  const getPremiumLessons = async () => {
+    const snapshot = await getDocs(
+      query(collection(db, "lessons"), where("premium", "==", true), orderBy("updatedAt", "desc"))
+    );
     const allLessons = snapshot.docs.map((lesson) => lesson.data());
     console.log(allLessons);
-    setLessons(allLessons);
+    setPremiumLessons(allLessons);
   };
 
   if (!fetched) {
     setFetched(true);
-    getAllLessons();
+    getPremiumLessons();
   }
 
   return (
@@ -38,6 +50,13 @@ function AllLessons() {
         </li>
 
         {lessons.map((lesson) => (
+          <li key={lesson.slug}>
+            <Link href={`/lessons/${lesson.slug}`}>
+              <a>{lesson.title}</a>
+            </Link>
+          </li>
+        ))}
+        {premiumLessons.map((lesson) => (
           <li key={lesson.slug}>
             <Link href={`/lessons/${lesson.slug}`}>
               <a>{lesson.title}</a>
