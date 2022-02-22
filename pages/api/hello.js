c6199b08546aa1075bd1cd5892655d12877ae98c
Andreas Börjesson <borjesson.andreas@gmail.com>
2022-02-22 06:20:42 +0100

    WIP stripe

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
+import { Stripe, loadStripe } from '@stripe/stripe-js';
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 }
