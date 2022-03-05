ffde013cac49f463f876dc449f5aeab442d1fffe
Andreas Börjesson <borjesson.andreas@gmail.com>
2022-03-05 13:39:55 +0100

    Now creating customer in stripe before sending to checkout. And linking firebase and stripe customers.

--- pages/api/go-to-checkout.js
+++ pages/api/go-to-checkout.js
@@ -1,9 +1,19 @@
-import { withAuth } from "@lib/be/firebaseAdmin";
+import { withAuth, db } from "@lib/be/firebaseAdmin";
+import { getOrCreateCustomer } from "@belib/customers";
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 async function handler(req, res) {
-  console.log("Go-to-checkout request received");
+  const { currentUser } = req;
+
+  const firebaseUser = await db.collection("users").doc(currentUser.uid).get();
+
+  if (!firebaseUser.exists) {
+    return res.status(404).json({});
+  }
+
+  const customer = await getOrCreateCustomer(currentUser.uid);
+  console.log(customer);
 
   const session = await stripe.checkout.sessions.create({
     line_items: [
@@ -12,12 +22,11 @@ async function handler(req, res) {
         quantity: 1,
       },
     ],
+    customer: customer.id,
     mode: "subscription",
     success_url: "http://localhost:3000/enter?success=true&session_id={CHECKOUT_SESSION_ID}",
     cancel_url: "http://localhost:3000/enter?canceled=true",
   });
-
-  console.log(session);
 
   return res.status(200).json({ sessionId: session.id, url: session.url });
 }
